b11751b2e41d99d79e35fe339fed8326a29e3558
Weston Isheim <wisheim@gmail.com>
2021-09-16 15:56:15 -0600

    done

--- part-1/server/controllers/auth.js
+++ part-1/server/controllers/auth.js
@@ -1,4 +1,7 @@
+const bcrypt = require('bcryptjs')
+
 const users = []
+
 
 module.exports = {
     login: (req, res) => {
@@ -6,8 +9,14 @@ module.exports = {
       console.log(req.body)
       const { username, password } = req.body
       for (let i = 0; i < users.length; i++) {
-        if (users[i].username === username && users[i].password === password) {
-          res.status(200).send(users[i])
+        if (users[i].username === username) {
+          const check = bcrypt.compareSync(password, users[i].passwordHash)
+          if (checked){
+            let userToReturn = {...users[i]}
+            delete userToReturn.passwordHash
+            res.status(200).send(userToReturn)
+            return
+          }
         }
       }
       res.status(400).send("User not found.")
@@ -15,7 +24,44 @@ module.exports = {
     register: (req, res) => {
         console.log('Registering User')
         console.log(req.body)
+        const {username, email, firstName, lastName, password} = req.body
         users.push(req.body)
-        res.status(200).send(req.body)
-    }
+
+        for (let i = 0; i < users.length; i++) {
+          if (users[i].username === username) {
+            res.status(400).send("try loging in")
+            return 
+          }
+          
+        }
+        
+
+        for(let i = 0; i < users.length; i++){
+          const existing = bcypt.compareSync(password, users[i].passwordHash)
+          if(existing){
+            users[i].newUser.push(newUser)
+            let passwordToReturn ={...users[i]}
+            delete passwordToReturn.passwordHash
+            res.status(200).send(passwordToReturn)
+            return
+          };
+        
+        const salt = bcrypt.genSaltSync(5)
+        const passwordHash = bcrypt.hashSync(password, salt)
+
+        let newUser = {
+          passwordHash,
+          username,
+          email,
+          firstName,
+          lastName
+        };
+
+        users.push(newUser)
+        let userToReturn = {...newUser}
+        delete userToReturn.passwordHash
+        res.status(200).send(passwordToReturn)
+        console.log(users)
+    } 
+  }
 }
